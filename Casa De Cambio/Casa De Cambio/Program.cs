661952a5e0aad262e53580a115c9c49b87ebe78a
FallenShadow17 <55670052+FallenShadow17@users.noreply.github.com>
2022-01-27 13:51:32 -0800

    basic start for the program.

---
+++ Casa De Cambio/Casa De Cambio/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Casa_De_Cambio
+{
+    class Program
+    {
+
+        class Employee
+        {
+            int workID;
+            string name;
+            
+            //Setters
+            void SetWorkID(int i)
+            {
+                workID = i;
+            }
+            void SetName(string s)
+            {
+                name = s;
+            }
+
+            //Getters
+            int GetWorkID()
+            {
+                return workID;
+            }
+
+            string GetName()
+            {
+                return name;
+            }
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            // create start up for program.
+            int option = 1;
+            while(option != 1 || option != 2)
+            {
+                Console.WriteLine("1. new employee\n2. existing employee\n");
+                option = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("input: {0}", option);
+            }
+
+
+            //make a main interface.
+            Console.WriteLine("Casa de cambio");
+        }
+    }
+}
